aa2cff37f6b816fb661d18d3ef0a7d32429acb42
Martin D'Agostino <martin.dagostino@klarna.com>
2022-09-15 22:47:21 +0200

    impro testing and extending

--- test/bot_test.go
+++ test/bot_test.go
@@ -1,12 +1,8 @@
 package test
 
 import (
-	"encoding/json"
-	"github.com/margostino/climateline-processor/api"
-	"github.com/margostino/climateline-processor/domain"
 	"github.com/stretchr/testify/require"
 	"net/http"
-	"net/http/httptest"
 	"os"
 	"strings"
 	"testing"
@@ -65,124 +61,86 @@ func TestSingleShowInput(t *testing.T) {
 }
 
 func TestShowAllInput(t *testing.T) {
-	message := &BotRequest{
-		UpdateId: 1,
-		Message: &BotMessage{
-			MessageId: 1,
-			Text:      "show",
-			From: &BotFrom{
-				Id:        1,
-				FirstName: "mock.name",
-				Username:  "mock.username",
-			},
-			Chat: &BotChat{
-				Id: 1,
-			},
-		},
-	}
+	message := mockBotMessageRequest("show")
 
 	request, err := mockBotRequest(message, os.Getenv("TELEGRAM_BOT_SECRET"))
-	if err != nil {
-		t.Fatal(err)
-	}
+	assertError(err, t)
 
 	items := mockItems("mock.title")
 
-	cacheServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "GET" {
-			t.Errorf("unexpected method for fetching caching: got %v want %v", r.Method, "GET")
-		}
-		if r.URL.Query().Get("ids") != "*" {
-			t.Errorf("unexpected method for fetching caching: got %v want %v", r.URL.Query().Get("ids"), "*")
-		}
-		w.WriteHeader(http.StatusOK)
-		res, err := json.Marshal(items)
-		w.Write(res)
-		require.NoError(t, err)
-	}))
+	queries := make(map[string]string)
+	queries["ids"] = "*"
+	cacheServer := NewMockServer().
+		withMethod(http.MethodGet).
+		withStatus(http.StatusOK).
+		withBody(items).
+		withQueries(queries).
+		start(t)
 
-	os.Setenv("CACHE_BASE_URL", cacheServer.URL)
+	os.Setenv("CACHE_BASE_URL", cacheServer.Url)
 
-	response := httptest.NewRecorder()
-	handler := http.HandlerFunc(api.Bot)
-
-	handler.ServeHTTP(response, &request)
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if status := response.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
-
-	var botResponse domain.BotResponse
-	err = json.NewDecoder(response.Body).Decode(&botResponse)
-
-	if botResponse.ChatId != 1 {
-		t.Errorf("handler returned unexpected chat ID: got %v want %v", botResponse.ChatId, 1)
-	}
-
-	if !strings.Contains(botResponse.Text, "ID: mock.id") {
-		t.Errorf("handler returned unexpected chat ID: got %v want %v", botResponse.Text, "One item with ID: mock.id")
-	}
+	response := call(&request)
+	botResponse, err := parseBotResponse(response)
+	require.NoError(t, err)
+	assertStatus(response, http.StatusOK, t)
+	assertResponse(botResponse.ChatId != 1, botResponse.ChatId, "*", t)
+	assertResponse(!strings.Contains(botResponse.Text, "ID: mock.id"), botResponse.Text, "One item with ID: mock.id", t)
 }
 
 func TestCleanInput(t *testing.T) {
-	message := &BotRequest{
-		UpdateId: 1,
-		Message: &BotMessage{
-			MessageId: 1,
-			Text:      "clean",
-			From: &BotFrom{
-				Id:        1,
-				FirstName: "mock.name",
-				Username:  "mock.username",
-			},
-			Chat: &BotChat{
-				Id: 1,
-			},
-		},
-	}
+	message := mockBotMessageRequest("clean")
 
 	request, err := mockBotRequest(message, os.Getenv("TELEGRAM_BOT_SECRET"))
-	if err != nil {
-		t.Fatal(err)
-	}
+	assertError(err, t)
 
-	cacheServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "DELETE" {
-			t.Errorf("unexpected method for fetching caching: got %v want %v", r.Method, "DELETE")
-		}
-		w.WriteHeader(http.StatusOK)
-		require.NoError(t, err)
-	}))
+	cacheServer := NewMockServer().
+		withMethod(http.MethodDelete).
+		withStatus(http.StatusOK).
+		start(t)
 
-	os.Setenv("CACHE_BASE_URL", cacheServer.URL)
+	os.Setenv("CACHE_BASE_URL", cacheServer.Url)
 
-	response := httptest.NewRecorder()
-	handler := http.HandlerFunc(api.Bot)
+	response := call(&request)
+	botResponse, err := parseBotResponse(response)
+	require.NoError(t, err)
+	assertStatus(response, http.StatusOK, t)
+	assertResponse(botResponse.ChatId != 1, botResponse.ChatId, 1, t)
+	assertResponse(!strings.Contains(botResponse.Text, "cache deleted"), botResponse.Text, "One item with ID: cache deleted", t)
+}
 
-	handler.ServeHTTP(response, &request)
+func TestFetchInput(t *testing.T) {
+	message := mockBotMessageRequest("fetch")
 
-	if err != nil {
-		t.Fatal(err)
-	}
+	request, err := mockBotRequest(message, os.Getenv("TELEGRAM_BOT_SECRET"))
+	assertError(err, t)
 
-	if status := response.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
+	items := mockItems("mock.title")
+	cacheServer := NewMockServer().
+		withMethod(http.MethodPost).
+		withStatus(http.StatusOK).
+		withBody(items).
+		start(t)
 
-	var botResponse domain.BotResponse
-	err = json.NewDecoder(response.Body).Decode(&botResponse)
+	feeds := mockRssFeed()
+	feedServer := NewMockServer().
+		withMethod(http.MethodGet).
+		withStatus(http.StatusOK).
+		withBody(feeds).
+		start(t)
 
-	if botResponse.ChatId != 1 {
-		t.Errorf("handler returned unexpected chat ID: got %v want %v", botResponse.ChatId, 1)
-	}
+	jobServer := NewMockServer().
+		withMethod(http.MethodGet).
+		withStatus(http.StatusOK).
+		start(t)
 
-	if !strings.Contains(botResponse.Text, "cache deleted") {
-		t.Errorf("handler returned unexpected chat ID: got %v want %v", botResponse.Text, "One item with ID: cache deleted")
-	}
+	os.Setenv("CACHE_BASE_URL", cacheServer.Url)
+	os.Setenv("FEED_URL", feedServer.Url)
+	os.Setenv("JOB_BASE_URL", jobServer.Url)
+
+	response := call(&request)
+	botResponse, err := parseBotResponse(response)
+	require.NoError(t, err)
+	assertStatus(response, http.StatusOK, t)
+	assertResponse(botResponse.ChatId != 1, botResponse.ChatId, 1, t)
+	assertResponse(!strings.Contains(botResponse.Text, "Completed successfully"), botResponse.Text, "Completed successfully", t)
 }
