bde62f03927a23f3c3d9e6f511663413ac769e06
Martin D'Agostino <martin.dagostino@klarna.com>
2022-09-19 14:59:53 +0200

    debugging

--- config/sheet.go
+++ config/sheet.go
@@ -5,6 +5,7 @@ import (
 	"github.com/margostino/climateline-processor/common"
 	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
+	"log"
 	"os"
 	"strconv"
 )
@@ -22,9 +23,14 @@ func GetUrls(category string) []string {
 
 		if !common.IsError(err, "unable to retrieve data from sheet") && len(resp.Values) > 0 {
 			for _, row := range resp.Values {
-				println("sarlanga: %s", row[1].(string))
-				isEnabled, err := strconv.ParseBool(row[2].(string))
-				common.SilentCheck(err, "when fetching feed urls configuration")
+				var isEnabled bool
+				if len(row) == 3 {
+					isEnabled, err = strconv.ParseBool(row[2].(string))
+					common.SilentCheck(err, "when fetching feed urls configuration")
+				} else {
+					log.Printf("Configuration sheet for Feed Urls is not valid. It must have 3 columns. It has %d\n", len(row))
+				}
+
 				matchCategory := category != "*" && category == row[1]
 
 				if matchCategory || (isEnabled && category == "*") {
